ffb81a47cc5457d4ae43f8b5fd166513954d4965
Jose Ferreyra <joshk44@icloud.com>
2025-06-12 05:45:03 +0200

    Fix issues

--- shared/src/iosMain/kotlin/com/joseferreyra/ocr_kmm/ocr/OCRProcessorFactory.kt
+++ shared/src/iosMain/kotlin/com/joseferreyra/ocr_kmm/ocr/OCRProcessorFactory.kt
@@ -3,9 +3,9 @@ package com.joseferreyra.ocr_kmm.ocr
 actual class OCRProcessorFactory {
     actual fun create(): OCRProcessor {
         return object : OCRProcessor {
-            override suspend fun processImage(image: ByteArray): String {
+            override suspend fun processImage(image: ByteArray): List<String> {
                 // Placeholder implementation for Android
-                return "Processed image on IOS"
+                return listOf("","")
             }
         }
     }
